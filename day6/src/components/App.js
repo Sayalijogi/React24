527154d327efd3abb7c5f97e28fe60023c18f5a6
Sayalijogi <sayali.jogi2002@gmail.com>
2024-09-03 20:03:21 +0530

    commit

--- day6/src/components/App.js
+++ day6/src/components/App.js
@@ -1,49 +1,52 @@
-import {useState} from "react";
-import Logo from './Logo';
-import Form from './Form';
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
 
-export default function App(){
+export default function App() {
+  const [items, setItems] = useState([]);
 
-  const [items,setItems] = useState([])
-
-  //handler to add items
-  function handleAddItems(item){
-    setItems((items)=>[...items,item])
+  // handler  to add the item
+  // item {id, description , quantity ,packed}
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
   }
 
-  //handler to delete item
-  function handleDeleteItem(id){
-    setItems((items)=>items.filter((item)=>item.id != id))
+  // delete the item from list
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
   }
 
-  //to check which item is packed and unpacked / to check and uncheck the item
-  function handleToggleItem(id){
-    setItems((items)=>
-    items.map((item)=>
-    item.id === id ? {...item,packed:!item.packed}:item
-  ));
+  // to check which item is packed or unpacked
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
   }
 
-  //handler to delete all the items
-  function handleClearList(){
-    const confirmed = window.confirm("Are you want to delete the alert ?")
-    if(confirmed){
-      setItems([]);
-    }
+  // to clear all the list
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
+
+    if (confirmed) setItems([]);
   }
 
-  return(
+  return (
     <div className="app">
-      <Logo></Logo>
-      <Form onAddItems ={handleAddItems}></Form>
+      <Logo />
+      <Form onAddItems={handleAddItems} />
       <PackingList
-      items = {items}
-      onDeleteItem = {handleDeleteItem}
-      onToggleItem = {handleToggleItem}
-      onClearList = {handleClearList}>
-      </PackingList>
-      <Stats items={items}></Stats>
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
     </div>
   );
 }
-
--- day6/src/components/Form.js
+++ day6/src/components/Form.js
@@ -1,43 +1,43 @@
-import {useState} from "react";
+import { useState } from "react";
+export default function Form({ onAddItems }) {
 
-export default function form({onAddItems}){
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-    const [description,setDescription] = useState("");
-    const [quantity,setQuantity] = useState(1);
+  // handler code
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit(e){
-        e.preventDefault();
+    if (!description) return;
 
-        if(!description) {
-            return 
-        };
+    const newItem = { description, quantity, packed: false, id: Date.now() };
 
-        const newItem = {description,quantity,packed:false,id:Date.now()}
-        onAddItems(newItem)
+    onAddItems(newItem);
 
-        setDescription('')
-        setQuantity(1)
-    }
-    //jsx
+    setDescription("");
+    setQuantity(1);
+  }
 
-    return (
-        <form className="add-form" onSubmit={handleSubmit}>
-            <h3>What you need for trip ?</h3>
-        <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
-        {
-        Array.from({length:20},(_,i) => i + 1).map((num)=>(
-            <option value={num} key={num}>
-                {num}
-            </option>
-        ))
-        }
-        </select>
-        <input type="text" 
-        placeholder="Item..." 
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
         value={description}
-         onChange={(e)=>setDescription(e.target.value)}>
-         </input>
-         <button>Add</button>
-        </form>
-    );
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
 }
